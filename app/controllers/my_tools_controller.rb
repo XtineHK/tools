bd867b6d00fef5304bb86a575ab57b5e3cfb7597
Brandon Ruaux <ruaux.brandon@gmail.com>
2020-11-17 11:19:19 +0100

    All routes created

--- app/controllers/my_tools_controller.rb
+++ app/controllers/my_tools_controller.rb
@@ -1,44 +1,41 @@
 class MyToolsController < ApplicationController
   def index
-    @my_tool = MyTool.all
+    @my_tools = current_user.tools
+    #Tool.where(user: current_user)
   end
 
   def show
-    @my_tool = MyTool.find(params[:id])
-  end
-
-  def show
-    @my_tool = MyTool.find(params[:id])
+    #@my_tool = MyTool.find(params[:id])
   end
 
   def new
-    @my_tool = MyTool.new
+    #@my_tool = MyTool.new
   end
 
   def create
-    @my_tool = MyTool.new(my_tool_params)
-    my_tool.save
-    redirect_to my_tool_path(@my_tool)
+    # @my_tool = MyTool.new(my_tool_params)
+    # my_tool.save
+    # redirect_to my_tool_path(@my_tool)
   end
 
   def edit
-    @my_tool = MyTool.find(params[:id])
+    #@my_tool = MyTool.find(params[:id])
   end
 
   def update
-    my_tool = MyTool.find(params[:id])
-    my_tool.update(my_tool_params)
-    redirect_to my_tool_path(my_tool)
+    # my_tool = MyTool.find(params[:id])
+    # my_tool.update(my_tool_params)
+    # redirect_to my_tool_path(my_tool)
   end
 
   def destroy
-    my_tool = MyTool.find(params[:id])
-    MyTool.destroy
+    # my_tool = MyTool.find(params[:id])
+    # MyTool.destroy
   end
 
   private
 
   def my_tool_params
-    params.require(:my_tool).permit(:daily_price, :location, :description, :name, photo:[])
+    #params.require(:my_tool).permit(:daily_price, :location, :description, :name, photo:[])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :tools, only: [:index, :show]
-  resources :bookings, only: [:new, :create, :update, :edit]
+  resources :tools, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only:[] do
+    member do
+      patch :confirm
+      patch :refuse
+    end
+  end
   resources :my_tools
+  resource :profile, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
